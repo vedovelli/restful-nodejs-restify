4d2df8e0fb1904c45e4215c676854a997e55d018
Fabio Vedovelli <vedovelli@gmail.com>
2017-09-19 08:59:35 +0200

    Pequena correção numa variavel de ambiente para acesso ao db

--- src/services/mysql/tests/setup.js
+++ src/services/mysql/tests/setup.js
@@ -5,7 +5,7 @@ const mysqlServer = require('mysql')
 
 const connection = mysqlServer.createConnection({
   host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
+  user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_TEST_DATABASE
 })
