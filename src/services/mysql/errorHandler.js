127d990802de831378074288e4aca1c182f9ee07
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-10-26 15:13:50 -0200

    Refatorado mysql/index Para Extração do setup da conexão do banco de dados e Extração do método errorHandler para um módulo isolado.

---
+++ src/services/mysql/errorHandler.js
@@ -0,0 +1,9 @@
+
+const errorHandler = (error, msg, rejectFunction) => {
+  if (error) {
+    console.error(error)
+  }
+  rejectFunction({ error: msg })
+}
+
+module.exports = errorHandler
--- src/services/mysql/setup.js
+++ src/services/mysql/setup.js
@@ -1,15 +1,13 @@
 
-require('dotenv').config()
-
 const mysqlServer = require('mysql')
 
 const connection = mysqlServer.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_TEST_DATABASE
+  database: process.env.MYSQL_DATABASE
 })
 
-const errorHandler = require('../errorHandler')
+const errorHandler = require('./errorHandler')
 
 module.exports = { connection, errorHandler }
