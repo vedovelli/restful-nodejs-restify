a9308394d0cd6988256174aaf3b8ee7f3ddf3bc7
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-11-12 14:20:03 -0200

    Extraindo conexão e errorHandler de services/mysql/index.js para arquivos isolados. (setup.js e errorHandler.js)

--- src/services/mysql/index.js
+++ src/services/mysql/index.js
@@ -1,18 +1,5 @@
 
-const mysqlServer = require('mysql')
-
-const connection = mysqlServer.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-})
-
-const errorHandler = (error, msg, rejectFunction) => {
-  if (error) console.error(error)
-  rejectFunction({ error: msg })
-}
-
+const { connection, errorHandler } = require('./setup')
 const dependencies = { connection, errorHandler }
 
 const categoryModule = require('./categories')(dependencies)
